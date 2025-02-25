ad2d01edd3e2d3f468aac6f62275c2d3955d8567
Jake Zook <jakezook01@gmail.com>
2025-02-25 11:34:01 -0700

    Fixed submit bug.

--- scripts.js
+++ scripts.js
@@ -63,12 +63,6 @@ function sendReportEmail(reportData) {
 		day: "numeric",
 	});
 	const subject = `Daily Report - ${formattedDate}`;
-
-	// Collect base64 data from file inputs (for AM, MID, PM photo fields)
-	const amPhoto = document.getElementById("am-photo").dataset.base64 || "";
-	const midPhoto = document.getElementById("mid-photo").dataset.base64 || "";
-	const pmPhoto = document.getElementById("pm-photo").dataset.base64 || "";
-
 	emailjs
 		.send("service_ea6xww4", "template_8gr2wqn", {
 			to_email: "jake.zook@playactivate.com",
@@ -79,19 +73,18 @@ function sendReportEmail(reportData) {
 			"am-customer": reportData["am-customer"],
 			"am-maintenance": reportData["am-maintenance"],
 			"am-shoutouts": reportData["am-shoutouts"],
-			"am-photo": amPhoto, // Include the base64 image for AM
+
 			"mid-mod": reportData["mid-mod"],
 			"mid-general": reportData["mid-general"],
 			"mid-customer": reportData["mid-customer"],
 			"mid-maintenance": reportData["mid-maintenance"],
 			"mid-shoutouts": reportData["mid-shoutouts"],
-			"mid-photo": midPhoto, // Include the base64 image for MID
+
 			"pm-mod": reportData["pm-mod"],
 			"pm-general": reportData["pm-general"],
 			"pm-customer": reportData["pm-customer"],
 			"pm-maintenance": reportData["pm-maintenance"],
 			"pm-shoutouts": reportData["pm-shoutouts"],
-			"pm-photo": pmPhoto, // Include the base64 image for PM
 			"open-notes": reportData["open-notes"],
 		})
 		.then((response) => {
@@ -102,7 +95,7 @@ function sendReportEmail(reportData) {
 
 			// Optionally, reset the form fields if needed
 			document.getElementById("report-form").reset();
-			window.location.reload();
+			window.location.reload;
 		})
 		.catch((error) => {
 			console.error("Error submitting the report:", error);
